daa04874750148cbce16d5c7a3537754f6087b4c
Jiang Liyin <liyinjiang@cienet.com.cn>
2015-11-16 10:20:56 +0800

    Add framework target

---
+++ Demo/ALGReversedFlowLayout/ALGReversedFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  ALGReversedFlowLayout.h
+//  ALGReversedFlowLayout
+//
+//  Created by Lacie on 11/16/15.
+//  Copyright © 2015 Bloom FIlter. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ALGReversedFlowLayout.
+FOUNDATION_EXPORT double ALGReversedFlowLayoutVersionNumber;
+
+//! Project version string for ALGReversedFlowLayout.
+FOUNDATION_EXPORT const unsigned char ALGReversedFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALGReversedFlowLayout/PublicHeader.h>
+
+
